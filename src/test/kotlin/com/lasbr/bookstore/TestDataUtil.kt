5bbef660cfd9c6be79a7d379357f47a303c06ca1
Luciano Alves <lasbr_alves@outlook.com>
2025-02-05 19:47:58 -0300

    feat: added fun testAuthorEntity to run test in service layer. Also added services/impl/AuthorServiceImplTest.

--- src/test/kotlin/com/lasbr/bookstore/TestDataUtil.kt
+++ src/test/kotlin/com/lasbr/bookstore/TestDataUtil.kt
@@ -1,8 +1,17 @@
 package com.lasbr.bookstore
 
 import com.lasbr.bookstore.domain.dto.AuthorDto
+import com.lasbr.bookstore.domain.entities.AuthorEntity
 
 fun testAuthorDtoA(id: Int? = null) = AuthorDto(
+    id = id,
+    name = "John Doe",
+    age = 30,
+    description = "Some Description",
+    image = "author-image.jpeg"
+)
+
+fun testAuthorEntityA(id: Int? = null) = AuthorEntity(
     id = id,
     name = "John Doe",
     age = 30,
